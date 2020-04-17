2a2c368108a738a3b3c7f2d4502e4c78a188d8ed
thfgid1106 <thfgid1106@gmail.com>
2020-04-17 19:04:29 +0900

    코드 작성

--- run.py
+++ run.py
@@ -1,17 +1,25 @@
 # -*- coding:utf-8 -*-
 
 import os
-from flask import Flask, render_template
-from models import db
+from flask import Flask, render_template, request
+from models import *
 
 
 
 app = Flask(__name__)
 
-
-@app.route('/a')
+@app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/login', methods=["POST"])
+def login():
+    userid = request.form["userid"]
+    password = request.form["password"]
+    print(userid)
+    print(password)
+    return "Success, login"
 
 
 if __name__ == "__main__":
@@ -24,7 +32,9 @@ if __name__ == "__main__":
 
     db.init_app(app)
     db.app = app
-    db.create_all()
+    with app.app_context():
+        db.create_all()
+
     app.run(host="127.0.0.1", port = '5000', debug=True)
 
 
